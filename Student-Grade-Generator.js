0333ee95efc7ea87f9d79ceed6d773b1d8846ea1
Presbury <wamaepresbury034@gmail.com>
2025-03-13 14:51:47 +0300

    updated code

---
+++ Student-Grade-Generator.js
@@ -0,0 +1,27 @@
+function getStudentGrade(marks){
+    // let marks = prompt("Enter student marks (0-100):");
+    // marks = parseInt(marks);
+
+    if(isNaN(marks) || marks < 0 || marks > 100){
+        console.log("Invalid input! Marks should be a number between 0 and 100.");
+        return;
+    }
+    let grade;
+    if (marks > 79) {
+        grade = "A";
+    } 
+    else if (marks >= 60) { 
+        grade = "B";
+    }
+    else if (marks >= 50) {
+        grade = "C";
+    }
+    else if (marks >= 40) {
+        grade = "D";
+    }
+    else {
+        grade = "E";
+    }
+    console.log(`The student's grade is: ${grade}`);
+}
+getStudentGrade(102);
