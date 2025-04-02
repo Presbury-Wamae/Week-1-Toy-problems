0cb6dd9a371d31017c8634027a776d574afe6c07
Presbury <wamaepresbury034@gmail.com>
2025-04-02 11:53:00 +0300

    Add Cylinder class to calculate volume with four decimal places

--- Bonus.js
+++ Bonus.js
@@ -20,3 +20,19 @@ function steps(n) {
     }
   }
   
+//Write a JavaScript program to get the volume of a Cylinder with four decimal places using object classes.
+//Volume of a cylinder : V = πr2h where r is the radius and h is the height of the cylinder.
+
+class Cylinder {
+    constructor(radius, height) {
+      this.radius = radius;
+      this.height = height;
+    }
+    getVolume() {
+      const volume = Math.PI * Math.pow(this.radius, 2) * this.height;
+      return parseFloat(volume.toFixed(4)); 
+    }
+  }
+  const myCylinder = new Cylinder(5, 10); 
+const volume = myCylinder.getVolume();
+console.log(`Volume of the cylinder: ${volume}`)
